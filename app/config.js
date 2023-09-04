75167e0ef15d5516db08e6f696df07553cf48737
Jorge Estrada Machado <jorgitoestrada2924@gmail.com>
2023-09-04 11:39:37 -0400

    init curso fast web

--- app/config.js
+++
@@ -1,2 +0,0 @@
-export const API_URL = process.env.API_URL || "https://strapi-embici-ruta.onrender.com/api";
-export const STRAPI_URL = process.env.STRAPI_URL || "http://127.0.0.1:1337"; 
---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+
+export default async function HomePage() {
+  
+  return (
+    <div className='flex justify-center items-center h-[50vh] p-4'>
+      <div className=' bg-blue flex w-full h-full justify-center items-center' >
+        <h1>Curso Fast Web</h1>
+      </div>
+    </div>
+  )
+}
